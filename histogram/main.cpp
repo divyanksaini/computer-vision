da7af1ef463a7ae28f23d5cb1357bb97babe7743
Divyank saini <76219000+divyanksaini@users.noreply.github.com>
2022-04-27 10:32:09 +0530

    Add files via upload

---
+++ histogram/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+
+    Mat image;
+    image = cv::imread("C:\\Users\\divya\\Desktop\\cpp\\opencv\\lena.jpg");
+
+    vector<Mat> channels;
+
+    split(image, channels);
+
+	int hsize = 256;
+
+    float range[] = { 0, 256 }; 
+	const float* histranges[] = { range };
+
+    bool uniform = true, accumulate = false;
+
+    Mat b, g, r;
+    calcHist( &channels[0], 1, 0, Mat(), b, 1,  &hsize, histranges, uniform, accumulate );
+
+    calcHist( &channels[1], 1, 0, Mat(), g, 1,  &hsize, histranges, uniform, accumulate );
+
+    calcHist( &channels[2], 1, 0, Mat(), r, 1,  &hsize, histranges, uniform, accumulate );
+
+    int hist_w= 512, hist_h=400;
+    int bin_w = cvRound( (double) hist_w/hsize );
+    Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
+
+    normalize(b, b, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+    normalize(g, g, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+    normalize(r, r, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+
+    for( int i = 1; i < hsize; i++ )
+    {
+        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b.at<float>(i-1)) ),
+              Point( bin_w*(i), hist_h - cvRound(b.at<float>(i)) ),
+              Scalar( 255, 0, 0), 2, 8, 0  );
+        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g.at<float>(i-1)) ),
+              Point( bin_w*(i), hist_h - cvRound(g.at<float>(i)) ),
+              Scalar( 0, 255, 0), 2, 8, 0  );
+        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(r.at<float>(i-1)) ),
+              Point( bin_w*(i), hist_h - cvRound(r.at<float>(i)) ),
+              Scalar( 0, 0, 255), 2, 8, 0  );
+    }
+    imshow("Source image", image );
+    imshow("calcHist Demo", histImage );
+    imwrite("C:\\Users\\divya\\Desktop\\cpp\\opencv\\result.jpg",histImage);
+    cv::waitKey(0);
+    cv::destroyAllWindows();
+
+}
